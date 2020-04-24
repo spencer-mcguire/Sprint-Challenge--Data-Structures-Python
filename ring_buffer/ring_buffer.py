635704bb6696887f0df99b9225c678a856919bac
spencer-mcguire <smcguire1826@gmail.com>
2020-04-24 11:05:31 -0500

    get

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -30,6 +30,23 @@ class RingBuffer:
         list_buffer_contents = []
 
         # TODO: Your code here
+        # grab the start of the ring 
+        # add that node to the list so that is the first thing
+        #check where the start is in the ring. if there is a next set that as the next node, if not set the next node is the head
+        # loop until there are no more nodes left to display 
+        start = self.current
+        list_buffer_contents.append(start.value)
+        if start.next is not None:
+            next_node = start.next
+        else:
+            next_node = self.storage.head
+
+        while next_node != start:
+            list_buffer_contents.append(next_node.value)
+            if next_node.next is not None:
+                next_node = next_node.next
+            else:
+                next_node = self.storage.head
 
         return list_buffer_contents
 
